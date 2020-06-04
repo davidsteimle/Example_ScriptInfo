fb5ea4d2ea2f201836f42adc73f08ddbd828e808
David B. Steimle <davidsteimle@gmail.com>
2020-06-03 22:25:31 -0400

    Adding files.

---
+++ MyOtherOtherProject.ps1
@@ -0,0 +1,49 @@
+
+<#PSScriptInfo
+
+.VERSION 1.0
+
+.GUID 9031b997-9f0b-4db2-af32-d7d4c4a0166f
+
+.AUTHOR Steimle, David B. (davidsteimle@gmail.com)
+
+.COMPANYNAME United States Postal Service
+
+.COPYRIGHT 
+
+.TAGS 
+
+.LICENSEURI 
+
+.PROJECTURI https://github.com/davidsteimle/Example_ScriptInfo
+
+.ICONURI 
+
+.EXTERNALMODULEDEPENDENCIES 
+
+.REQUIREDSCRIPTS 
+
+.EXTERNALSCRIPTDEPENDENCIES 
+
+.RELEASENOTES
+
+#>
+
+<# 
+
+.DESCRIPTION 
+Buy stamps! 
+.SYNOPSIS
+Just an example...
+.EXAMPLE
+MyOtherOtherProject.ps1
+.EXAMPLE
+MyOtherOtherProject.ps1 -Debug
+.EXAMPLE
+MyOtherOtherProject.ps1 | ConvertTo-Json
+#> 
+Param(
+    [switch]$Debug # Would provide additional information if this were a real script.
+)
+
+Get-Date
---
+++ MyOtherProject.ps1
@@ -0,0 +1,41 @@
+
+<#PSScriptInfo
+
+.VERSION 1.0
+
+.GUID d15896ff-629e-49cd-8d06-0069c39e0ca6
+
+.AUTHOR Steimle, David B. (davidsteimle@gmail.com)
+
+.COMPANYNAME United States Postal Service
+
+.COPYRIGHT 
+
+.TAGS 
+
+.LICENSEURI 
+
+.PROJECTURI https://github.com/davidsteimle/Example_ScriptInfo
+
+.ICONURI 
+
+.EXTERNALMODULEDEPENDENCIES 
+
+.REQUIREDSCRIPTS 
+
+.EXTERNALSCRIPTDEPENDENCIES 
+
+.RELEASENOTES
+
+
+#>
+
+<# 
+
+.DESCRIPTION 
+ Buy stamps! 
+
+#> 
+Param()
+
+
