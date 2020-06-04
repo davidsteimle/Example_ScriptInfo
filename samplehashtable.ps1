b3dbcb4a82d59ca97fe7936b01fd26c10315a80a
David B. Steimle <davidsteimle@gmail.com>
2020-06-03 22:38:50 -0400

    add file

---
+++ samplehashtable.ps1
@@ -0,0 +1,7 @@
+$MyOtherProject = @{
+    Path = ".\MyOtherProject.ps1"
+    Author = "Steimle, David B. (davidsteimle@gmail.com)"
+    CompanyName = "United States Postal Service"
+    Description = "Buy stamps!"
+    ProjectUri = "https://github.com/davidsteimle/Example_ScriptInfo"
+}
